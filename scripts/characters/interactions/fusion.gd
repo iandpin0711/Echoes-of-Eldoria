af06a49b18cbb06f1089c70833a4685aaa30905c
Alejandro Lorenzo <alormel012@g.educaand.es>
2026-04-21 10:45:16 +0200

    Refactor player character scenes and animations

--- scripts/characters/interactions/fusion.gd
+++ scripts/characters/interactions/fusion.gd
@@ -1,67 +1,58 @@
 extends CharacterBody2D
 
-const SPEED = 250.0
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
-var starting_position: Vector2
+var char_index = 0
+var is_busy = false
 
-@onready var warrior_scene = preload("res://scenes/Player/player1.tscn")
-@onready var lancer_scene  = preload("res://scenes/Player/player2.tscn")
-@onready var archer_scene  = preload("res://scenes/Player/player3.tscn")
+const SPEED = 200.0
+const CHARACTERS = [
+	"res://animations/warrior.tres",
+	"res://animations/lancer.tres",
+	"res://animations/archer.tres",
+]
 
+func _ready():
+	load_character()
 
-func _ready() -> void:
-	starting_position = global_position
+func _physics_process(_delta):
+	if is_busy:
+		return
 
+	if Input.is_action_just_pressed("attack"):
+		is_busy = true
+		sprite.play("attack")
+		await sprite.animation_finished
+		is_busy = false
 
-func _physics_process(delta: float) -> void:
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	elif Input.is_action_just_pressed("interact"):
+		char_index = (char_index + 1) % CHARACTERS.size()
+		load_character()
 
-	var direction = Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * SPEED
-		$AnimatedSprite2D.flip_h = direction < 0
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		var dir = Vector2.ZERO
 
-	move_and_slide()
+		if Input.is_action_pressed("right"):
+			dir.x = 1
+		elif Input.is_action_pressed("left"):
+			dir.x = -1
 
-	if Input.is_action_just_pressed("interact"):
-		separate()
+		if Input.is_action_pressed("down"):
+			dir.y = 1
+		elif Input.is_action_pressed("up"):
+			dir.y = -1
 
+		if dir != Vector2.ZERO:
+			velocity = dir.normalized() * SPEED
+			move_and_slide()
+			if dir.x != 0:
+				sprite.flip_h = dir.x < 0
+			sprite.play("run")
+			
+		else:
+			velocity = Vector2.ZERO
+			sprite.play("idle")
 
-func separate() -> void:
-	var warrior_inst = warrior_scene.instantiate()
-	var lancer_inst  = lancer_scene.instantiate()
-	var archer_inst  = archer_scene.instantiate()
-
-	warrior_inst.global_position = global_position
-	lancer_inst.global_position  = global_position + Vector2(-35, -20)
-	archer_inst.global_position  = global_position + Vector2(35, -20)
-
-	warrior_inst.is_active = true
-	lancer_inst.is_active  = false
-	archer_inst.is_active  = false
-
-	warrior_inst.modulate.a = 1.0
-	lancer_inst.modulate.a  = 0.5
-	archer_inst.modulate.a  = 0.5
-
-	var world = get_parent()
-	world.add_child(warrior_inst)
-	world.add_child(lancer_inst)
-	world.add_child(archer_inst)
-
-	warrior_inst.companions = [lancer_inst, archer_inst]
-	lancer_inst.companions  = [warrior_inst, archer_inst]
-	archer_inst.companions  = [warrior_inst, lancer_inst]
-
-	if warrior_inst.has_node("Camera2D"):
-		warrior_inst.get_node("Camera2D").make_current()
-
-	queue_free()
-
-
-func reset_to_start() -> void:
-	global_position = starting_position
-	velocity = Vector2.ZERO
+func load_character():
+	sprite.sprite_frames = load(CHARACTERS[char_index])
+	sprite.play("idle")
